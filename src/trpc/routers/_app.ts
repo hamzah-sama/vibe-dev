98f0fa9d437a0288644fe3125095d6e9ba524aee
Hamzah Sama <hamzah.sama@gmail.com>
2025-10-23 09:53:08 +0700

    03: agent tools

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -3,12 +3,12 @@ import { baseProcedure, createTRPCRouter } from "../init";
 import z from "zod/v3";
 export const appRouter = createTRPCRouter({
   invoke: baseProcedure
-    .input(z.object({ text: z.string() }))
+    .input(z.object({ value: z.string() }))
     .mutation(async ({ input }) => {
       await inngest.send({
-        name: "test/hello.world",
+        name: "run/code.agent",
         data: {
-          text: input.text,
+          value: input.value,
         },
       });
     }),
